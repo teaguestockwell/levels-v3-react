4dadfb43b290a92bf9b8502ca935e3936f872d22
teagueStockwell <tsAppDevelopment@gmail.com>
2021-05-08 13:50:23 -0700

    clock grid refrac

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {Col, Row, Space} from 'antd'
-import {useEffect, useRef, useState} from 'react'
+import {Col, Row} from 'antd'
+import {useEffect, useState} from 'react'
 import {Typography} from 'antd'
 import {format} from 'date-fns'
 import {getUTCDate} from '../util'
 import {Const} from '../const'
-import {v4} from 'uuid'
 
 const {Text} = Typography
 
 export const Clock = () => {
   const [lnow, setNow] = useState(new Date())
   const znow = getUTCDate(lnow)
+
   useEffect(() => {
     const time = setTimeout(() => setNow(new Date()), 1000)
     return () => clearTimeout(time)
   }, [lnow])
 
-  const df = {
+  const clock = {
     lHMS: format(lnow, 'HH:mm:ss'),
     lJJJ: format(lnow, 'DDD'),
     lYMD: format(lnow, 'yyyy-LL-dd'),
@@ -32,12 +32,10 @@ export const Clock = () => {
     color = '#000000',
     textAlign: any = 'center'
   ) => (
-    <Col key={v4()} span={5}>
+    <Col span={6} style={{display: 'inline-flex', justifyContent: 'center'}}>
       <Text
         style={{
-          textAlign,
           color,
-          //fontFamily: 'DM Sans',
           fontWeight: 'normal',
           fontSize: '14px',
           lineHeight: '18px',
@@ -48,44 +46,35 @@ export const Clock = () => {
     </Col>
   )
 
-  return (
-    <div
-      style={{
-        boxShadow: Const.BOX_SHADOW,
-        paddingBottom: '10px',
-        paddingTop: '10px',
-      }}
-    >
-      <Row justify="center">
-        <Col flex={2} />
-        {[
-          getCol('Zone', Const.COLORS.TXT_DISABLED, 'left'),
-          getCol('hh:mm:ss', Const.COLORS.TXT_DISABLED),
-          getCol('JJJ', Const.COLORS.TXT_DISABLED),
-          getCol('YYYY-MM-DD', Const.COLORS.TXT_DISABLED),
-        ]}
-        <Col flex={2} />
-      </Row>
-      <Row justify="center">
-        <Col flex={2} />
-        {[
-          getCol('Local', Const.COLORS.TXT_RED, 'left'),
-          getCol(df.lHMS),
-          getCol(df.lJJJ),
-          getCol(df.lYMD),
-        ]}
-        <Col flex={2} />
-      </Row>
-      <Row justify="center">
-        <Col flex={2} />
-        {[
-          getCol('Zulu', Const.COLORS.TXT_RED, 'left'),
-          getCol(df.zHMS),
-          getCol(df.zJJJ),
-          getCol(df.zYMD),
-        ]}
-        <Col flex={2} />
-      </Row>
-    </div>
-  )
+  const rowProps = {
+    justify: 'center',
+    type: 'flex',
+  } as any
+
+  return <div
+    style={{
+      boxShadow: Const.BOX_SHADOW,
+      paddingBottom: '10px',
+      paddingTop: '10px',
+    }}
+  >
+    <Row {...rowProps}>
+      {getCol('Zone', Const.COLORS.TXT_DISABLED,'left')}
+      {getCol('hh:mm:ss', Const.COLORS.TXT_DISABLED)}
+      {getCol('JJJ', Const.COLORS.TXT_DISABLED)}
+      {getCol('YYYY-MM-DD', Const.COLORS.TXT_DISABLED)}
+    </Row>
+    <Row {...rowProps}>
+      {getCol('Local', Const.COLORS.TXT_RED, 'left')}
+      {getCol(clock.lHMS)}
+      {getCol(clock.lJJJ)}
+      {getCol(clock.lYMD)}
+    </Row>
+    <Row {...rowProps}>
+      {getCol('Zulu', Const.COLORS.TXT_RED, 'left')}
+      {getCol(clock.zHMS)}
+      {getCol(clock.zJJJ)}
+      {getCol(clock.zYMD)}
+    </Row>
+  </div>
 }
