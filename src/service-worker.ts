0288bd2e9d0637288fc29411a9a9fae11d858e06
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-07 14:31:38 -0700

    bump sw cache name

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -76,7 +76,7 @@ registerRoute(
   url.origin === self.location.origin && 
   url.pathname.endsWith('lastUpdated'),
   new StaleWhileRevalidate({
-    cacheName: 'json',
+    cacheName: 'lastUpdated',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
