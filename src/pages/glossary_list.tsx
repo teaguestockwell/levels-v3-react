cc8d4151e94208513bd4855bc948dad2cb654c49
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-07 21:28:10 -0700

    fix text overflow

--- src/pages/glossary_list.tsx
+++ src/pages/glossary_list.tsx
@@ -4,12 +4,12 @@ import { AircraftDeep } from '../types/aircraftDeep'
 export const GlossaryList = () => {
   const air = useUserAir() as AircraftDeep
 
-  return <div style={{paddingLeft: 14,paddingRight: 14, textAlign: 'left'}}>
+  return <div style={{marginLeft: 14,marginRight: 14, textAlign: 'left'}}>
     {
       air.glossarys.map(g => {
-        return <div key={g.glossaryId} style={{paddingTop: 28}}>
-          <div style={{display: 'flex', fontWeight: 'bold',fontSize: '2rem'}}>{g.name}</div>
-          <div style={{display: 'flex', textAlign:'left',fontSize: '1rem'}}>{g.body}</div>
+        return <div key={g.glossaryId} style={{marginTop: 28}}>
+          <div style={{fontWeight: 'bold',fontSize: 20, overflowWrap: 'break-word'}}>{g.name}</div>
+          <div style={{textAlign:'left',fontSize: 14,overflowWrap: 'break-word'}}>{g.body}</div>
           </div>
       })
     }
