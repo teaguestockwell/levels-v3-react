524a8bb11d0a53292bd1396ade0f2b9e42d3a65a
teagueStockwell <tsAppDevelopment@gmail.com>
2021-09-29 21:17:48 -0700

    Init show work

--- src/components/get_mac_button.test.tsx
+++ src/components/get_mac_button.test.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {waitFor} from '@testing-library/react'
-import {GetMacButton} from './get_mac_button'
+import {render, waitFor,screen} from '@testing-library/react'
+import {filterKeys, GetMacButton, ModalOpen} from './get_mac_button'
 import {renderWrapped} from '../testUtils/render_wrapped'
 import {userStore} from '../hooks/user_store'
 import * as Types from '../types'
+import { getPerMac } from '../utils/util'
+import { mockAircraftsDeep } from '../testUtils/mock_aircrafts_deep'
 
 
 
@@ -26,5 +28,148 @@ describe('GetMacButton', () => {
       expect(ct.queryAllByText('Loading Test').length).toBe(0)
     )
     expect(ct.getByText('%MAC: Invalid')).toBeInTheDocument()
+  })
+
+  it('shows work', async () => {
+    const c17aER = mockAircraftsDeep[0]
+    const cargoStrings: Types.CargoString[] = [
+      {
+        uuid: '0',
+        name: 'Basic Aircraft',
+        weightEA: '282000',
+        fs: '922',
+        qty: '1',
+        category: Types.CargoCategory.BasicAircraft,
+        isValid: true,
+      },
+      {
+        uuid: '1',
+        name: 'Tank 1',
+        weightEA: '250', // 1C-17A-5-2 2-29: tank1: 250lb, 28 simp mom
+        fs: '1120', // = 28 simple mom * 10,000 simple moment modifier / 250
+        qty: '1',
+        category: Types.CargoCategory.Tank,
+        isValid: true,
+      },
+      {
+        uuid: '2',
+        name: 'Tank 2 ER',
+        weightEA: '25750', // 1C-17A-5-2 2-32: tank 2 ER: 25750lb, 2053 simp mom
+        fs: '797.281553398', // = 2053 simple mom * 10,000 simple moment modifier / 25750
+        qty: '1',
+        category: Types.CargoCategory.Tank,
+        isValid: true,
+      },
+      {
+        uuid: '3',
+        name: 'Tank 3 ER',
+        weightEA: '4500', // 1C-17A-5-2 2-29: tank 3 ER: 4500lb, 380 simp mom
+        fs: '844.44444444444444', // = 380 simple mom * 10,000 simple moment modifier / 4500
+        qty: '1',
+        category: Types.CargoCategory.Tank,
+        isValid: true,
+      },
+      {
+        uuid: '4',
+        name: 'Tank 4',
+        weightEA: '36750', // 1C-17A-5-2 2-34: tank 4: 36750lb, 3,636 simp mom
+        fs: '989.387755102', // = 3,636 simple mom * 10,000 simple moment modifier / 36750
+        qty: '1',
+        category: Types.CargoCategory.Tank,
+        isValid: true,
+      },
+    ]
+
+    const permac = getPerMac(c17aER, cargoStrings)
+
+    expect(permac).toStrictEqual({
+      qtyGrandTotal: '5.00',
+      momentMultiplier: '10000.00',
+      mac: '309.50',
+      lemac: '793.60',
+      balArm: '919.04',
+      momentGrandTotal: '320974000.00',
+      simpleMomentGrandTotal: '32097.40',
+      weightGrandTotal: '349250.00',
+      percentMacDecimal: '0.4053',
+      percentMacPercent: '40.53%',
+      items: [
+        {
+          uuid: '0',
+          name: 'Basic Aircraft',
+          weightEA: '282000.00',
+          fs: '922',
+          qty: '1',
+          category: 'BasicAircraft',
+          momentEach: '260004000.00',
+          simpleMomentEach: '26000.40',
+          weightTotal: '282000.00',
+          momentTotal: '260004000.00',
+          simpleMomentTotal: '26000.40',
+          isValid: true,
+        },
+        {
+          uuid: '1',
+          name: 'Tank 1',
+          weightEA: '250.00',
+          fs: '1120',
+          qty: '1',
+          category: 'Tank',
+          momentEach: '280000.00',
+          simpleMomentEach: '28.00',
+          weightTotal: '250.00',
+          momentTotal: '280000.00',
+          simpleMomentTotal: '28.00',
+          isValid: true,
+        },
+        {
+          uuid: '2',
+          name: 'Tank 2 ER',
+          weightEA: '25750.00',
+          fs: '797.281553398',
+          qty: '1',
+          category: 'Tank',
+          momentEach: '20530000.00',
+          simpleMomentEach: '2053.00',
+          weightTotal: '25750.00',
+          momentTotal: '20530000.00',
+          simpleMomentTotal: '2053.00',
+          isValid: true,
+        },
+        {
+          uuid: '3',
+          name: 'Tank 3 ER',
+          weightEA: '4500.00',
+          fs: '844.44444444444444',
+          qty: '1',
+          category: 'Tank',
+          momentEach: '3800000.00',
+          simpleMomentEach: '380.00',
+          weightTotal: '4500.00',
+          momentTotal: '3800000.00',
+          simpleMomentTotal: '380.00',
+          isValid: true,
+        },
+        {
+          uuid: '4',
+          name: 'Tank 4',
+          weightEA: '36750.00',
+          fs: '989.387755102',
+          qty: '1',
+          category: 'Tank',
+          momentEach: '36360000.00',
+          simpleMomentEach: '3636.00',
+          weightTotal: '36750.00',
+          momentTotal: '36360000.00',
+          simpleMomentTotal: '3636.00',
+          isValid: true,
+        },
+      ],
+    })
+
+    render(<ModalOpen perMac={permac} cb={() => {return}}/>)
+
+    expect(screen.getByText('40.53%')).toBeInTheDocument()
+
   })
 })
--- src/components/get_mac_button.tsx
+++ src/components/get_mac_button.tsx
@@ -1,23 +1,134 @@
 import {getUserAir, useCargos} from '../hooks/user_store'
 import {getPerMac} from '../utils/util'
+import {Modal, Table } from 'antd'
+import { capitalizeFirst } from '../utils/util'
+import { CargoCategory } from '../types'
+import React from 'react'
+import * as Types from '../types'
+import { v4 } from 'uuid'
+
+export const filterKeys = [
+  'isValid',
+  // 'category',
+  'uuid',
+]
+
+const sortingOrder: Record<CargoCategory, number> = {
+  [CargoCategory.BasicAircraft]: 0,
+  [CargoCategory.Tank]: 1,
+  [CargoCategory.Steward]: 2,
+  [CargoCategory.Emergency]: 3,
+  [CargoCategory.Extra]: 4,
+  [CargoCategory.User]: 5,
+}
+
+export const ModalOpen = ({perMac, cb}: {perMac: Types.PercentMac, cb: () => void}) => {
+  perMac.items.sort((a,b) => {
+    if(a.category === b.category) {
+      return a.name.localeCompare(b.name)
+    }
+    return sortingOrder[a.category] - sortingOrder[b.category]
+  })
+
+  const getCol = (t:string) => <div style={{width: 100, textAlign: 'center'}}>{t}</div>
+  
+
+  return <Modal 
+  title="Show Work"
+  bodyStyle={{paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 15}}
+  visible={true}
+  footer={null}
+  closable={true}
+  onCancel={() => cb()}
+  width="100%"
+  centered
+>
+  <Table
+    key={v4()}
+    style={{paddingTop: 0}} 
+    pagination={false}
+    columns={
+      Object.keys(perMac.items[0])
+      .filter(k => !filterKeys.includes(k))
+      .map(k => ({
+        title: capitalizeFirst(k),
+        dataIndex: k,
+        key: k,
+      }))
+    }
+    dataSource={perMac.items}
+    scroll={{x: true}}
+  />
+  <div style={{marginLeft: 10, marginRight: 10}}>
+  <div style={{overflowX: 'auto', display: 'flex', flexDirection: 'column', marginTop: 30}}>
+    <div style={{textAlign: 'center', width: '100%', fontWeight: 600, fontSize: 18}}>Balance Arm</div>
+
+    <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      {getCol('Balance Arm')}
+      {getCol('=')}
+      {getCol('Moment')}
+      {getCol('/')}
+      {getCol('Total Weight')}
+    </div>
+
+    <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      {getCol(perMac.balArm)}
+      {getCol('=')}
+      {getCol(perMac.momentGrandTotal)}
+      {getCol('/')}
+      {getCol(perMac.weightGrandTotal)}
+    </div>
+  </div>
+
+  <div style={{overflowX: 'auto', display: 'flex', flexDirection: 'column', marginTop: 30}}>
+    <div style={{textAlign: 'center', width: '100%', fontWeight: 600, fontSize: 18}}>Mean Aerodynamic Chord %</div>
+
+    <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      {getCol('MAC%')}
+      {getCol('=')}
+      {getCol('Balance Arm')}
+      {getCol('-')}
+      {getCol('LEMAC')}
+      {getCol('/')}
+      {getCol('MAC')}
+    </div>
+
+    <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      {getCol(perMac.percentMacPercent)}
+      {getCol('=')}
+      {getCol(perMac.balArm)}
+      {getCol('-')}
+      {getCol(perMac.lemac)}
+      {getCol('/')}
+      {getCol(perMac.mac)}
+    </div>
+  </div>
+  </div>
+
+  
+</Modal>
+}
 
 export const GetMacButton = () => {
   const cargos = useCargos()
   const isCargoValid = cargos.every((c) => c.isValid)
   const air = getUserAir()
   const calculation = isCargoValid ? getPerMac(air, cargos) : null
+  const [open, setOpen] = React.useState(false)
+  const text =  isCargoValid ? `Show Work: ${calculation?.percentMacPercent}` : '%MAC: Invalid'
 
-  const onClick = () =>
-    isCargoValid
-      ? alert(JSON.stringify(calculation))
-      : () => {
-          return
-        }
+  const onClick = () => {
+    if(isCargoValid){
+      setOpen(true)
+    }
+  }
 
   return (
+    <>
     <div style={{marginTop: 50, marginBottom: 60, display: 'flex', alignItems: 'center',justifyContent: 'center'}}>
       <div
       className="hover-button"
+      onClick={onClick}
         style={{
           background: '#06BA7F',
           color: '#fff',
@@ -31,12 +142,11 @@ export const GetMacButton = () => {
           textAlign: 'center',
           lineHeight: '60px',
         }}
-        onClick={onClick}
-      >
-        {isCargoValid
-          ? `Show Work: ${calculation?.percentMacPercent}`
-          : '%MAC: Invalid'}
-      </div>
+      >{text}</div>
     </div>
+    {
+      open && calculation && <ModalOpen perMac={calculation} cb={() => setOpen(false)} />
+    }
+    </>
   )
 }
